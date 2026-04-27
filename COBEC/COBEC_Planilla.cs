d62c45a2399db2bbed5047d5f72c39a07f5e2cca
camposrenzo <camposr@royalsystems.net>
2026-04-27 15:04:58 -0500

    documentacion 20260427

--- COBEC/COBEC_Planilla.cs
+++ COBEC/COBEC_Planilla.cs
@@ -6,10 +6,24 @@ namespace COBEC
 {
     public class COBEC_Planilla
     {
+        /// <summary>Codigo de la compania ejemplo: 01000000 = EDO SOKO S.A.C. </summary>  
+        /// <example>01000000</example>
         public string compania { get; set; }
+
+        /// <summary>Periodo en formato yyyyMMdd ejemplo: 20161212</summary>
+        /// <example>20161212</example>
         public string periodo { get; set; }
+
+        /// <summary>Codigo Tipo Proceso ejemplo: NO0 = Planilla Mensual</summary>
+        /// <example> NO0 </example>
         public string tipoProceso { get; set; }
+
+        /// <summary>Codigo Tipo Planilla ejemplo: EM = Empleados</summary>
+        /// <example> EM </example>
         public string tipoPlanilla { get; set; }
+
+        /// <summary>Codigo Empleado ejemplo: 10017 = KANASHIRO ARAKAKI, NATALIA VERONICA </summary>
+        /// <example> 10017 </example>
         public string empleado { get; set; }
     }
 }
--- COBEC/PlanillaResponse.cs
+++ COBEC/PlanillaResponse.cs
@@ -9,28 +9,55 @@ namespace COBEC
     /// </summary>
     public class PlanillaResponse
     {
-        /// <summary>Código del empleado -->Ejemplo: EMP001</summary>        
+        /// <summary>Código del empleado -->Ejemplo: 10017</summary>        
         public string Codigo { get; set; }
 
-        /// <summary>Nombre completo del empleado  -->Ejemplo:Walter ROman</summary>        
+        /// <summary>Nombre completo del empleado  -->Ejemplo:KAN**** A****, NATA** VER****</summary>        
         public string NombreEmpleado { get; set; }
 
-        /// <summary>Tipo de Plantilla  -->Ejemplo: M = Mensual / A=  </summary>     
+        /// <summary>Tipo de Plantilla  -->Ejemplo: EM</summary>     
         public string TipoPlanilla { get; set; }
-       
+
+        /// <summary>Descripcion Planilla  -->Ejemplo: EM = Empleados</summary>   
         public string DescripcionPlanilla { get; set; }
+
+        /// <summary>Tipo Documento  -->Ejemplo: D: D.N.I </summary>  
         public string TipoDocumento { get; set; }
+
+        /// <summary>Numero Documento  -->Ejemplo: 0**5*5*5 </summary>  
         public string NumeroDocumento { get; set; }
+
+        /// <summary>Codigo cargo  -->Ejemplo: 38 </summary>  
         public string CodigoCargo { get; set; }
+
+        /// <summary>Descripcion cargo  -->Ejemplo: 38: ASISTENTE DE FINANZAS </summary> 
         public string DescripcionCargo { get; set; }
+
+        /// <summary>Sucursal  -->Ejemplo: OFICINA PRINCIPAL </summary>   
         public string Sucursal { get; set; }
+
+        /// <summary>Centro costo  -->Ejemplo: General Logística</summary> 
         public string CentroCosto { get; set; }
+
+        /// <summary>Fecha ingreso  -->Ejemplo: 2009-06-01T00:00:00 </summary> 
         public DateTime? FechaIngreso { get; set; }
+
+        /// <summary>Fecha cese  -->Ejemplo: 2009-06-01T00:00:00 </summary> 
         public DateTime? FechaCese { get; set; }
+
+        /// <summary>Tipo contrato  -->Ejemplo: IN </summary> 
         public string TipoContrato { get; set; }
+
+        /// <summary>Descripcion Tipo contrato  -->Ejemplo: IN : A Plazo Indeterminado </summary> 
         public string DescripcionTipoContrato { get; set; }
+
+        /// <summary>Codigo AFP  -->Ejemplo: 04</summary> 
         public string CodigoAFP { get; set; }
+
+        /// <summary>Nombre AFP  -->Ejemplo: 04: Prima</summary> 
         public string NombreAFP { get; set; }
+
+        /// <summary>Numero AFP  -->Ejemplo: 56**0**KA**1</summary> 
         public string NumeroAFP { get; set; }
         public decimal? Sueldo { get; set; }
         public int? DiasTrabajados { get; set; }
@@ -48,12 +75,17 @@ namespace COBEC
     // Clase para cada item del detalle
     public class PlanillaDetalleItem
     {
+        /// <summary>Concepto -->Concepto--> codigo que identifica el concepto , la descripcion se encuentra en Texto Impresion  </summary> 
         public string Concepto { get; set; }
         public decimal? Monto { get; set; }
         public decimal? Cantidad { get; set; }
         public decimal? Saldo { get; set; }
+
+        /// <summary>Tipo concepto-->Ejemplo: IN:Ingresos | DE:Deducciones </summary> 
         public string TipoConcepto { get; set; }
         public int? PlanillaOrden { get; set; }
+
+        /// <summary>Texto Impresion --> Descripcion del concepto ejemplo: 0100:GRATIFICACION ORDINARIA | 0200:Vacaciones  </summary> 
         public string TextoImpresion { get; set; }
     }
 }
